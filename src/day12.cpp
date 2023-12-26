43410c33dbe5456acd725152d0f3b2a3dd27a3b4
konmenel <konmenel@gmail.com>
2023-12-26 12:30:20 +0200

    Day 12 Part 2 finally

--- src/day12.cpp
+++ src/day12.cpp
@@ -7,7 +7,10 @@
 #include <string>
 #include <numeric>
 #include <algorithm>
-#include <unordered_map>
+#include <array>
+#include <assert.h>
+
+#include "robin_hood.h"
 
 
 typedef std::vector<std::string> input_t;
@@ -20,6 +23,7 @@ typedef  int8_t  i8;
 typedef  int16_t i16;
 typedef  int32_t i32;
 typedef  int64_t i64;
+typedef size_t usize;
 
 #ifdef DEBUG
     #define debug_println(fmt, ...) std::cout << std::format((fmt), ##__VA_ARGS__) << std::endl
@@ -28,8 +32,6 @@ typedef  int64_t i64;
     #define debug_println(fmt, ...)
     #define debug_print(fmt, ...)
 #endif
-
-
 
 
 void usage(const std::string &program_name) {
@@ -108,13 +110,13 @@ void applyPerm(const std::string &condition, u64 perm, std::string &permuted_str
 }
 
 
-u32 countValidPerm(const std::string &condition, const std::vector<u32> &counts) {
+u64 countValidPerm(const std::string &condition, const std::vector<u32> &counts) {
     u32 sum = 0;
     const u32 unknowns = countUknowns(condition);
     std::string tmp = condition;
 
     debug_println("Unknown condition {}:", condition);
-    for (u64 perm = 0; perm < (1 << unknowns); perm++) {
+    for (u64 perm = 0; perm < (1U << unknowns); perm++) {
         applyPerm(condition, perm, tmp);
         std::vector<u32> perm_counts = countContiguous(tmp);
         if (perm_counts == counts) {
@@ -132,13 +134,73 @@ u32 countValidPerm(const std::string &condition, const std::vector<u32> &counts)
 }
 
 
+struct Record {
+    usize i;    // index in condition
+    usize ci;   // index in count vector
+    usize bs;   // current broken block size
+
+    Record(usize val=0) : i(val), ci(val), bs(val) {}
+    Record(usize i, usize ci, usize bs) : i(i), ci(ci), bs(bs) {}
+
+    inline bool operator==(const Record &rhs) const {
+        return i == rhs.i && ci == rhs.ci && bs == rhs.bs;
+    }
+};
+
+
+template<>
+struct std::hash<Record> {
+    inline std::size_t operator()(const Record &rec) const noexcept {
+        return (rec.i << 32) + (rec.ci << 16) + rec.bs;
+    }
+};
+
+
+robin_hood::unordered_flat_map<Record, u64> cache;
+
+
+u64 countValidPermCached(const std::string &cond, const std::vector<u32> &counts, Record rec) {
+    if (cache.contains(rec))
+        return cache[rec];
+
+    if (rec.i == cond.size()) {
+        if (rec.ci == counts.size() && rec.bs == 0) {
+            return 1;
+        }
+        if (rec.ci == counts.size()-1 && rec.bs == counts[rec.ci]) {
+            return 1;
+        }
+        return 0;
+    }
+
+    u64 ret = 0;
+    for (char c : std::array<char,2>{'.', '#'}) {
+        if (cond[rec.i] == '?' || cond[rec.i] == c) {
+            if (c == '.' && rec.bs > 0 && rec.ci < counts.size() && counts[rec.ci] == rec.bs) {
+                ret += countValidPermCached(cond, counts, Record(rec.i+1, rec.ci+1, 0));
+            } else if (c == '.' && rec.bs == 0) {
+                 ret += countValidPermCached(cond, counts, Record(rec.i+1, rec.ci, 0));
+            } else if (c == '#') {
+                ret += countValidPermCached(cond, counts, Record(rec.i+1, rec.ci, rec.bs+1));
+            }
+        }
+    }
+    cache[rec] = ret;
+    return ret;
+}
+
+
 u64 part1(const input_t &in) {
     u64 totalArrangements = 0;
     for (const auto &line : in) {
         std::string condition;
         std::vector<u32> counts;
         getRecordAndCounts(line, condition, counts);
+        // totalArrangements += countValidPermCached(condition, counts, Record(0, 0, 0));
         totalArrangements += countValidPerm(condition, counts);
+        
+        cache.clear();
+        assert(countValidPerm(condition, counts) == countValidPermCached(condition, counts, Record()));
     }
 
     return totalArrangements;
@@ -148,18 +210,21 @@ u64 part1(const input_t &in) {
 u64 part2(const input_t &in) {
     u64 totalArrangements = 0;
     for (const auto &line : in) {
-        std::string condition1;
+        cache.clear();
+        std::string condition;
         std::vector<u32> counts;
-        getRecordAndCounts(line, condition1, counts);
-        std::string condition2 = "?" + condition1;
-        std::string condition3 = condition1 + "?";
-        std::string condition4 = "?" + condition1 + "?";
-        
-        const uint n1 = countValidPerm(condition1, counts);
-        const uint n2 = countValidPerm(condition2, counts);
-        const uint n3 = countValidPerm(condition3, counts);
-        const uint n4 = countValidPerm(condition4, counts);
-        totalArrangements += 0;
+        getRecordAndCounts(line, condition, counts);
+
+        std::string condition_unfolded = condition;
+        std::vector<u32> counts_unfolded = counts;
+        for (usize i = 0; i < 4; ++i) {
+            condition_unfolded += '?' + condition;
+            for (u32 el : counts) counts_unfolded.emplace_back(el);
+        }
+
+        u64 arr = countValidPermCached(condition_unfolded, counts_unfolded, Record(0, 0, 0));
+        totalArrangements += arr;
+        debug_println("{} - {}", line, arr);
     }
 
     return totalArrangements;
@@ -177,9 +242,9 @@ int main(int argc, char *argv[]) {
     std::cout << "-----PART 1-----\n";
     std::cout << "Total arrangements = " << res1 << std::endl;
     
-    // auto res2 = part2(lines);
-    // std::cout << "-----PART 2-----\n";
-    // std::cout << "Total arrangements = " << res2 << std::endl;
+    auto res2 = part2(lines);
+    std::cout << "-----PART 2-----\n";
+    std::cout << "Total arrangements = " << res2 << std::endl;
 
     return 0;
 }
